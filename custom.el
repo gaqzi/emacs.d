815512aef5830683b2029250b90b3eade1ff4f90
Björn Andersson <ba@sanitarium.se>
2012-08-11 06:35:44 +0200

    Proper fonts for OSX

--- custom.el
+++ custom.el
@@ -1,32 +1,36 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(autotest-use-ui t)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(display-time-24hr-format t)
+ '(display-time-mode t)
  '(edit-server-url-major-mode-alist (quote (("sanitarium.se" . markdown-mode))))
  '(markdown-italic-underscore t)
  '(markdown-xhtml-header-content "<meta http-equiv='content-type' content='text/html; charset=utf-8' />")
  '(show-paren-mode t)
+ '(tool-bar-mode nil)
  '(transient-mark-mode nil)
  '(weblogger-config-alist (quote (("sanitarium.se" "http://sanitarium.se/blog/xmlrpc.php" "admin" "" "1"))))
  '(weblogger-edit-mode (quote markdown-mode))
  '(weblogger-server-url "http://sanitarium.se/blog/")
  '(weblogger-server-username "admin"))
 
+;; Non Mac OS X GUI settings
 (if (not (or (eq window-system nil) (eq system-type 'darwin)))
     (progn
       (load (concat emacs-d-root "color-theme/themes/color-theme-sunburst.el"))
-      (color-theme-sunburst)))
+      (color-theme-sunburst)
       ;;(require 'color-theme)
       ;;(color-theme-initialize)
       ;;(color-theme-ld-dark)))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
+      (custom-set-faces
+       '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Droid Sans Mono")))))))
+
+;; OSX GUI Settings
+(if (or (not (eq window-system nil)) (eq system-type 'darwin))
+    (custom-set-faces
+     '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "Apple" :family "Monaco"))))))
