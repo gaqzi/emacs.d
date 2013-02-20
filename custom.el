dc6af904f97fcc10bf596395a5eb4c5c874b4e89
Björn Andersson <ba@sanitarium.se>
2013-02-20 07:39:43 +0100

    Fonts for both linux and mac

--- custom.el
+++ custom.el
@@ -1,8 +1,8 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(autotest-use-ui t)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
@@ -12,7 +12,6 @@
  '(markdown-italic-underscore t)
  '(markdown-xhtml-header-content "<meta http-equiv='content-type' content='text/html; charset=utf-8' />")
  '(show-paren-mode t)
- '(tool-bar-mode nil)
  '(transient-mark-mode nil)
  '(weblogger-config-alist (quote (("sanitarium.se" "http://sanitarium.se/blog/xmlrpc.php" "admin" "" "1"))))
  '(weblogger-edit-mode (quote markdown-mode))
@@ -28,9 +27,13 @@
       ;;(color-theme-initialize)
       ;;(color-theme-ld-dark)))
       (custom-set-faces
-       '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Droid Sans Mono")))))))
+       '(default ((t (:inherit nil :stipple nil :background "#111" :foreground "#ddd" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Droid Sans Mono"))))))
 
-;; OSX GUI Settings
-(if (or (not (eq window-system nil)) (eq system-type 'darwin))
-    (custom-set-faces
-     '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "Apple" :family "Monaco"))))))
+  ;; OSX GUI Settings
+  (if (or (not (eq window-system nil)) (eq system-type 'darwin))
+      (custom-set-faces
+       '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "Apple" :family "Monaco"))))))
+)
+
+
+
