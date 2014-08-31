4c0c53ad16f63d08bb584132b3e9b652f8e24190
Björn Andersson <ba@sanitarium.se>
2014-08-31 11:48:45 +0800

    Remove unnecessary parts of setup.sh

--- setup.sh
+++ setup.sh
@@ -4,24 +4,3 @@ if [ ! -f ~/.emacs ] ; then
 else
     echo "~/.emacs already exists, not creating"
 fi
-
-echo "Initializing submodules"
-git submodule init
-git submodule update
-
-echo "Initializing 'modes/ruby/rinari' submodules"
-(cd modes/ruby/rinari ; git submodule init && git submodule update)
-
-echo "Byte-compiling .el-files"
-files=(modes/ruby/cucumber/feature-mode.el)
-i=0
-while [ "$i" -lt "${#files[@]}" ] ; do
-    emacs -batch -eval "(byte-compile-file \"${files[$i]}\")"
-    i=`expr $i + 1`
-done
-
-# Python stuff
-echo "Remember to install python binaries:"
-echo <<EOF
-sudo pip install -e 'git://github.com/gaqzi/flake8.git#egg=flake8-1.5'
-EOF
